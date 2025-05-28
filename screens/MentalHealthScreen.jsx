873d34fc97cc59812496671e2c4806cbd1899a60
SEEDART007 <dashranger60@gmail.com>
2025-05-28 20:29:26 +0530

    add

--- screens/MentalHealthScreen.jsx
+++ screens/MentalHealthScreen.jsx
@@ -53,7 +53,7 @@ const MentalHealthScreen = () => {
       const response = await fetch('https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium', {
         method: 'POST',
         headers: {
-          Authorization: 'Bearer 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',
+          Authorization: 'Bearer {}',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
